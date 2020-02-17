53387850152c8fb041c7f27c3d081ddd8f93868d
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-02-17 20:18:44 +0000

    Basic 'all parts/index' page implemented

--- part/views.py
+++ part/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Import the Part model for use
+from .models import Part
+
 def index(request):
-    return render(request, 'allparts.html')
+    # Query the database and pull a set of all the objects contained
+    parts = Part.objects.all()
+    # Pass the 'parts' set to the 'allparts' template for display
+    return render(request, 'allparts.html', {'parts': parts})
