e40beaa56e307936c35bfa0291fe0d1875e51d37
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-02-19 18:29:29 +0000

    Added hrefs to main page

--- part/views.py
+++ part/views.py
@@ -8,4 +8,9 @@ def index(request):
     # Query the database and pull a set of all the objects contained
     parts = Part.objects.all()
     # Pass the 'parts' set to the 'allparts' template for display
-    return render(request, 'allparts.html', {'parts': parts})
+    return render(request, 'listparts.html', {'parts': parts})
+
+def single(request):
+    #parts = Part.objects.all()
+    parts = Part.objects.filter(name='bluebutton')
+    return render(request, 'listparts.html', {'parts': parts})
